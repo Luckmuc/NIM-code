37ce2ee796fce392b40615ca6b6212d58a25fd72
Luckmuc <valentin@dombret.de>
2026-07-17 11:17:29 +0200

    add a tiny health test

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express()
 const port = 6967
 
+app.get('/health-test', (req, res) => {
+    res.send("Server is healthy")
+})
+
+
 app.listen(port, () => {
     console.log(`server running on ${port}`)
 }
